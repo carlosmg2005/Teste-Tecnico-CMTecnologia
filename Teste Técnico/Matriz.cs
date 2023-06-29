a06ac047d992ae3beff09abe6fa4756e4560b2db
carlo <carlo@CARLOS>
2023-06-29 20:54:00 -0300

    Adicionar arquivos de projeto.

---
+++ Teste Técnico/Matriz.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste_Técnico
+{
+    public class Matriz
+    {
+        private int[,] matriz;
+
+        public Matriz(int[,] matriz)
+        {
+            this.matriz = matriz;
+        }
+
+        public void InverterDiagonais()
+        {
+            int tamanho = matriz.GetLength(0);
+
+            for (int i = 0; i < tamanho / 2; i++)
+            {
+                // Inverter a diagonal principal
+                TrocarElementos(i, i, tamanho - i - 1, tamanho - i - 1);
+
+                // Inverter a diagonal secundária
+                TrocarElementos(i, tamanho - i - 1, tamanho - i - 1, i);
+            }
+        }
+
+        private void TrocarElementos(int i1, int j1, int i2, int j2)
+        {
+            int temp = matriz[i1, j1];
+            matriz[i1, j1] = matriz[i2, j2];
+            matriz[i2, j2] = temp;
+        }
+
+        public void ImprimirMatriz()
+        {
+            int tamanho = matriz.GetLength(0);
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                for (int j = 0; j < tamanho; j++)
+                {
+                    Console.Write(matriz[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public int ContarSubmatriz(int[,] submatriz)
+        {
+            int count = 0;
+            int rowsA = matriz.GetLength(0);
+            int colsA = matriz.GetLength(1);
+            int rowsB = submatriz.GetLength(0);
+            int colsB = submatriz.GetLength(1);
+
+            for (int i = 0; i <= rowsA - rowsB; i++)
+            {
+                for (int j = 0; j <= colsA - colsB; j++)
+                {
+                    if (VerificarSubmatriz(i, j, submatriz))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private bool VerificarSubmatriz(int startRow, int startCol, int[,] submatriz)
+        {
+            int rowsB = submatriz.GetLength(0);
+            int colsB = submatriz.GetLength(1);
+
+            for (int i = 0; i < rowsB; i++)
+            {
+                for (int j = 0; j < colsB; j++)
+                {
+                    if (matriz[startRow + i, startCol + j] != submatriz[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Teste Técnico/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste_Técnico
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = 
+        {
+            {0, 1, 2, 3, 4},
+            {5, 6, 7, 8, 9},
+            {0, 1, 2, 3, 4},
+            {5, 6, 7, 8, 9},
+            {0, 1, 2, 3, 4}
+        };
+            int[,] submatriz = 
+        {
+            {2, 3},
+            {7, 6}
+        };
+
+            Matriz minhaMatriz = new Matriz(matriz);
+            Console.WriteLine("Matriz original:");
+            minhaMatriz.ImprimirMatriz();
+            Console.ReadLine();
+
+            minhaMatriz.InverterDiagonais();
+            Console.WriteLine("Matriz com diagonais invertidas:");
+            minhaMatriz.ImprimirMatriz();
+            Console.ReadLine();
+
+            Console.WriteLine("Submatriz B:");
+            Matriz subMatriz = new Matriz(submatriz);
+            subMatriz.ImprimirMatriz();
+            Console.ReadLine();
+
+            int count = minhaMatriz.ContarSubmatriz(submatriz);
+            Console.WriteLine("A submatriz B pode ser encontrada {0} vezes na matriz A.", count);
+            Console.ReadLine();
+        }
+    }
+}
